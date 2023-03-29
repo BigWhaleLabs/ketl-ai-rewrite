2728ae531878139ec75336787e0271ade1221710
T_DaMER <dpancov64@gmail.com>
2023-03-29 21:44:19 +0300

    Better rewrite query

--- src/controllers/rewrite.ts
+++ src/controllers/rewrite.ts
@@ -18,7 +18,7 @@ export default class LoginController {
       apiKey: env.OPEN_AI_API_KEY,
       completionParams: { model: 'gpt-4' }, // or 'gpt-4-0314'
       maxModelTokens: 8100, // not 32000 because it's not yet available via API
-      systemMessage: `You are GPT-4, a large language model trained by OpenAI. Rewrite this text as if you were ${Persona[persona]} without hashtags`,
+      systemMessage: `You are GPT-4, a large language model trained by OpenAI. Rewrite this text without extending too much as if you were ${Persona[persona]} without hashtags`,
     })
 
     const res = await api.sendMessage(text)
